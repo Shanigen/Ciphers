c5dd1874c89a5d1efe40b8ef03ff2a33cff73bf8
Shanigen <vkaraf@gmail.com>
2019-03-30 23:31:11 +0100

    Standard style correction

--- Decipher/affine_cipher.rb
+++ Decipher/affine_cipher.rb
@@ -1,12 +1,12 @@
-require '../Decipher/helper_functions'
+require "../Decipher/helper_functions"
 
 class AffineCipher
-  ALPHABET = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.freeze
+  ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".freeze
   LETTER_FREQUENCIES = {a: 8.167, b: 1.492, c: 2.782, d: 4.253, e: 12.702, f: 2.228,
                         g: 2.015, h: 6.094, i: 6.966, j: 0.153, k: 0.772, l: 4.025,
                         m: 2.406, n: 6.749, o: 7.507, p: 1.929, q: 0.095, r: 5.987,
                         s: 6.327, t: 9.056, u: 2.758, v: 0.978, w: 2.360, x: 0.150,
-                        y: 1.974, z: 0.074}.freeze
+                        y: 1.974, z: 0.074,}.freeze
   # Unnormalized IOC, normalization is performed like this: ioc_norm = ioc_unnorm/alphabet_len
   IOC = 1.73
 
@@ -16,7 +16,7 @@ class AffineCipher
   # @param [String] cipher_text
   def self.decipher(cipher_text)
     p cipher_text
-    File.open('affine_cipher', 'w') { |file|
+    File.open("affine_cipher", "w") { |file|
       alphabet_size = ALPHABET.size
 
       # Find all possible A's for alphabet length given in x
@@ -24,39 +24,35 @@ class AffineCipher
 
       # Find A' to A in mod x
       a_pairs = []
-      coprimes.each { |a|
-        _a = 1
-        while (_a * a) % alphabet_size != 1
-          _a += 1
-        end
-        a_pairs << [a, _a]
-      }
+      coprimes.each do |a|
+        a2 = 1
+        a2 += 1 while (a2 * a) % alphabet_size != 1
+        a_pairs << [a, a2]
+      end
 
-      #Transform letters from text to numbers
+      # Transform letters from text to numbers
       text_ids = []
       cipher_text.each_char { |c| text_ids << ALPHABET.index(c) }
 
-
-
-      #Brute force
+      # Brute force
       solutions = Hash.new(0)
-      a_pairs.each { |a, _a|
-        (1..alphabet_size).each { |b|
-          _text = HelperFunctions.affine_inverse ALPHABET, text_ids, _a, b
-          _text_freq = HelperFunctions.frequency_analysis(_text)
+      a_pairs.each do |a, a2|
+        (1..alphabet_size).each do |b|
+          text = HelperFunctions.affine_inverse ALPHABET, text_ids, a2, b
+          text_freq = HelperFunctions.frequency_analysis(text)
           freq_diff = 0
-          _text_freq.each_key { |c|
-            freq_diff += (LETTER_FREQUENCIES[c] - _text_freq[c]).abs
-          }
-          solutions[freq_diff] = [a, b, _text]
-        }
-      }
+          text_freq.each_key do |c|
+            freq_diff += (LETTER_FREQUENCIES[c] - text_freq[c]).abs
+          end
+          solutions[freq_diff] = [a, b, text]
+        end
+      end
 
       solutions = solutions.sort
 
-      solutions.each { |key, data|
+      solutions.each do |key, data|
         file.write("A: #{data[0]} B: #{data[1]} Text: #{data[2]} Frequency difference: #{key}\n")
-      }
+      end
     }
   end
 end
--- Decipher/vignere_cipher.rb
+++ Decipher/vignere_cipher.rb
@@ -1,12 +1,12 @@
-require '../Decipher/helper_functions'
+require "../Decipher/helper_functions"
 
 class VignereCipher
-  ALPHABET = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.freeze
+  ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".freeze
   LETTER_FREQUENCIES = {a: 8.167, b: 1.492, c: 2.782, d: 4.253, e: 12.702, f: 2.228,
                         g: 2.015, h: 6.094, i: 6.966, j: 0.153, k: 0.772, l: 4.025,
                         m: 2.406, n: 6.749, o: 7.507, p: 1.929, q: 0.095, r: 5.987,
                         s: 6.327, t: 9.056, u: 2.758, v: 0.978, w: 2.360, x: 0.150,
-                        y: 1.974, z: 0.074}.freeze
+                        y: 1.974, z: 0.074,}.freeze
   # Unnormalized IOC, normalization is performed like this: ioc_norm = ioc_unnorm/alphabet_len
   IOC = 1.73
 
@@ -15,64 +15,64 @@ class VignereCipher
     op_ids = []
 
     i = 0
-    text_ids.each { |id|
+    text_ids.each do |id|
       tmp = id - key_ids[i % key_ids.size]
       tmp = tmp >= 0 ? tmp : tmp + alphabet_len
       op_ids << tmp
       i += 1
-    }
+    end
 
-    return op_ids
+    op_ids
   end
 
   def self.decipher(cipher_text)
     len = cipher_text.size
     delta_iocs = Hash.new(0)
 
-    (1..20).each { |key_len|
+    (1..20).each do |key_len|
       delta_ioc = 0
-      (0...key_len).each { |col|
+      (0...key_len).each do |col|
         id = col
-        col_letters = ''
+        col_letters = ""
         while id < len
           col_letters << cipher_text[id]
           id += key_len
         end
         delta_ioc += HelperFunctions.ioc col_letters
-      }
+      end
       delta_iocs[key_len] = (delta_ioc / key_len).round 2, half: :down
-    }
+    end
 
     min_diff = Float::INFINITY
     best_key_len = 0
-    delta_iocs.each { |key_len, delta_ioc|
+    delta_iocs.each do |key_len, delta_ioc|
       diff = IOC - delta_ioc
       if diff.abs < min_diff
         min_diff = diff
         best_key_len = key_len
       end
-    }
+    end
 
     columns = []
-    (0...best_key_len).each { |col|
+    (0...best_key_len).each do |col|
       id = col
-      col_letters = ''
+      col_letters = ""
       while id < len
         col_letters << cipher_text[id]
         id += best_key_len
       end
       columns << col_letters
-    }
+    end
 
-    key = ''
-    columns.each { |col_text|
+    key = ""
+    columns.each do |col_text|
       text_ids = []
       best_correlation = 0
       best_b = -1
 
       col_text.each_char { |c| text_ids << ALPHABET.index(c) }
 
-      (0...ALPHABET.size).each { |b|
+      (0...ALPHABET.size).each do |b|
         frequencies = HelperFunctions.frequency_analysis HelperFunctions.affine_inverse ALPHABET, text_ids, 1, b
         correlation = 0
         frequencies.each { |c, n| correlation += LETTER_FREQUENCIES[c.downcase.to_sym] / 100 * n / 100 }
@@ -80,10 +80,10 @@ class VignereCipher
           best_correlation = correlation
           best_b = b
         end
-      }
+      end
 
       key << ALPHABET[best_b]
-    }
+    end
 
     puts "Key: #{key}"
 
@@ -94,7 +94,7 @@ class VignereCipher
 
     op_ids = VignereCipher.vignere_decryption ALPHABET.size, key_ids, text_ids
 
-    op_text = ''
+    op_text = ""
     op_ids.each { |id| op_text << ALPHABET[id] }
     puts "Message: #{op_text}"
   end
